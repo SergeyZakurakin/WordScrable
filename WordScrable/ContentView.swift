f44bbfc893fa02d19fb9a2766db336a24a3eebaf
SergeyZakurakin <pricezak@gmail.com>
2024-09-15 13:07:07 -0400

    finish project

--- WordScrable/ContentView.swift
+++ WordScrable/ContentView.swift
@@ -9,34 +9,112 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var usedWords: [String] = []
+    @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingErrors = false
     
     var body: some View {
         NavigationStack {
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
                 }
                 
                 Section {
                     ForEach(usedWords, id: \.self) { word in
-                        Text(word)
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
                     }
-                }
+                
             }
         }
-        .navigationTitle(newWord)
+        .navigationTitle(rootWord)
+        .onSubmit(addNewWord)
+        .onAppear(perform: startGame)
+        .alert(errorTitle, isPresented: $showingErrors) {
+            Button("OK") {}
+        } message: {
+            Text(errorMessage)
+        }
     }
+}
     
     private func addNewWord() {
         let answer = newWord.trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else { return }
         
+        guard isOriginal(word: answer) else {
+            wordError(title: "word used already", message: "Be more original")
+            return
+        }
         
-        usedWords.insert(answer, at: 0)
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You cant spell that word form '\(rootWord)'")
+                    return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "word not recognized", message: "You can't just make it")
+            return
+        }
+        
+        withAnimation{
+            usedWords.insert(answer, at: 0)
+        }
         newWord = ""
+    }
+    
+    private func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                print("Loaded words: \(allWords)")  // Отладочный вывод
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            } else {
+                print("faild to read")
+            }
+        }
+        fatalError("Could not load file start.txt form Bundle")
+    }
+    
+    private func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    private func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+            
+        }
+        return true
+    }
+    
+    private func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelRange.location == NSNotFound
+    }
+    
+    private func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingErrors = true
     }
     
 }
